94d03201e90a6bdd51c118b9d080d4603d4d5813
fredrik jorman <fredrik.jorman@gmail.com>
2026-08-28 16:03:18 +0200

    Add something

--- Sales.Report/Program.cs
+++ Sales.Report/Program.cs
@@ -32,3 +32,35 @@ for (int i = 0; i < productNames.Count; i++)
 {
     Console.WriteLine($"{productNames[i]}: {saleAmounts[i]} kr");
 }
+
+Console.WriteLine("Sammanfattning");
+Console.WriteLine($"Antal försäljningar: {productNames.Count}");
+
+Console.WriteLine($"Total försäljning: {saleAmounts.Sum()} kr");
+
+Console.WriteLine($"Genomsnittlig försäljning: {saleAmounts.Average()} kr");
+Console.WriteLine($"Största försäljningen: {saleAmounts.Max()} kr");
+
+
+
+var bigSales = saleAmounts.Where(s => s > 1000).Count();
+Console.WriteLine($"sales over 1000 kr: {bigSales}");
+
+var totalSale = saleAmounts.Sum();
+switch (totalSale)
+{
+    case < 5000:
+        Console.WriteLine("Resultat: Svag försäljningsdag");
+        break;
+
+    case >= 5000 and < 10000:
+        Console.WriteLine("Dagens försäljning är medel");
+        break;
+
+    case >= 10000:
+        Console.WriteLine("Dagens försäljning är hög");
+        break;
+
+        default:
+        break;
+}
