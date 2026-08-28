6832dfd03f21ac2302f48619196079773b98274d
fredrik jorman <fredrik.jorman@gmail.com>
2026-08-28 15:02:55 +0200

    Add product and amount to list.

--- Sales.Report/Program.cs
+++ Sales.Report/Program.cs
@@ -1,3 +1,23 @@
 ﻿List<string> productNames = [];
 List<decimal> saleAmounts = [];
 
+Console.WriteLine("Välkommen till försäljningsrapporten");
+Console.WriteLine();
+
+while (true)
+{
+    Console.WriteLine("Ange produktnamn");
+    string namn = Console.ReadLine();
+    Console.WriteLine("Ange försäljningsbeloppet");
+    string salesamount = Console.ReadLine();
+
+    if (decimal.TryParse(salesamount, out decimal result))
+    {
+        productNames.Add(namn);
+        saleAmounts.Add(result);
+    }
+    else
+    {
+        Console.WriteLine("incorrect amount");
+    }
+}    
